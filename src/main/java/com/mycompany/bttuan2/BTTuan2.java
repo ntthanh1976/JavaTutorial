4d8ac292267203605ebd450b38cda82b1271a5c7
ntthanh010176 <ntthanh010176@gmail.com>
2024-08-06 16:23:47 +0700

    phien ban 1

---
+++ src/main/java/com/mycompany/bttuan2/BTTuan2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bttuan2;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class BTTuan2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
